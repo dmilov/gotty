2bc4c4e7b4eb47e6994467bc1d4b179c3d658b85
dmilov <dmilov@users.noreply.github.com>
2021-01-13 16:28:43 +0200

    Update local deps to dmilov

--- app/app.go
+++ app/app.go
@@ -26,8 +26,8 @@ import (
 	"github.com/elazarl/go-bindata-assetfs"
 	"github.com/gorilla/websocket"
 	"github.com/kr/pty"
-	"github.com/yudai/hcl"
-	"github.com/yudai/umutex"
+	"github.com/dmilov/hcl"
+	"github.com/dmilov/umutex"
 )
 
 type InitMessage struct {
